20963714b1a7e288a22aae12c2dd40390315c076
Aayush Borkar <aayushb14@gmail.com>
2024-06-10 16:26:13 +0530

    Add comments

--- src/image1.cpp
+++ src/image1.cpp
@@ -4,21 +4,27 @@
 #include <iostream>
 
 int main() {
-    const int IMAGE_HEIGHT = 1200;
-    const int IMAGE_WIDTH = 1200;
+    // Image
+
+    const int IMAGE_HEIGHT = 720;
+    const int IMAGE_WIDTH = 720;
+
+    // Setup ProgressBar on std::clog
     ProgressBar bar(std::clog);
+
+    // Render
 
     std::cout << "P3\n"
               << IMAGE_WIDTH << " " << IMAGE_HEIGHT << "\n"
               << "255\n";
-    for (int i = 0; i < IMAGE_HEIGHT; i++) {
-        for (int j = 0; j < IMAGE_WIDTH; j++) {
+    for (int j = 0; j < IMAGE_HEIGHT; j++) {
+        for (int i = 0; i < IMAGE_WIDTH; i++) {
             auto pixel_color =
-                color(static_cast<double>(j) / (IMAGE_WIDTH - 1),
-                      static_cast<double>(i) / (IMAGE_HEIGHT - 1), 0.25);
+                color(static_cast<double>(i) / (IMAGE_WIDTH - 1),
+                      static_cast<double>(j) / (IMAGE_HEIGHT - 1), 0.25);
             write_color(std::cout, pixel_color);
         }
-        bar.update((static_cast<float>(i) + 1) / IMAGE_HEIGHT);
+        bar.update((static_cast<float>(j) + 1) / IMAGE_HEIGHT);
     }
     bar.completed();
 }
