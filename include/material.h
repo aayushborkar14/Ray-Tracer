bca3656a7c3aa251bf6901810b00c38587bbab12
Aayush Borkar <aayushb14@gmail.com>
2024-06-24 13:08:57 +0530

    Fuzzy metal

--- include/material.h
+++ include/material.h
@@ -41,16 +41,20 @@ class lambertian : public material {
 class metal : public material {
   private:
     color albedo;
+    double fuzz_factor;
 
   public:
-    metal(const color &a) : albedo(a) {}
+    metal(const color &a, double fuzz = 0)
+        : albedo(a), fuzz_factor(fuzz < 1 ? fuzz : 1) {}
 
     bool scatter(const ray &r_in, const hit_record &rec, color &attenuation,
                  ray &scattered) const override {
-        vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+        vec3 reflected =
+            unit_vector(reflect(unit_vector(r_in.direction()), rec.normal)) +
+            (fuzz_factor * random_unit_vector());
         scattered = {rec.p, reflected};
         attenuation = albedo;
-        return true;
+        return dot(scattered.direction(), rec.normal) > 0;
     }
 };
 
