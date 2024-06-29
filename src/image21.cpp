3a448c3e3e6b352ae8630e06ecef26f7e8fc8681
Aayush Borkar <aayushb14@gmail.com>
2024-06-29 09:38:16 +0530

    Add image21

---
+++ src/image21.cpp
@@ -0,0 +1,43 @@
+#include "camera.h"
+#include "hittable_list.h"
+#include "material.h"
+#include "sphere.h"
+#include "utils.h"
+#include "vec3.h"
+#include <memory>
+
+int main() {
+    // World
+    hittable_list world;
+
+    shared_ptr<lambertian> material_ground =
+        make_shared<lambertian>(color{0.8, 0.8, 0.0}, 0.8);
+    shared_ptr<lambertian> material_center =
+        make_shared<lambertian>(color{0.1, 0.2, 0.5}, 0.8);
+    shared_ptr<dielectric> material_left = make_shared<dielectric>(1.50);
+    shared_ptr<dielectric> material_bubble =
+        make_shared<dielectric>(1.00 / 1.50);
+    shared_ptr<metal> material_right =
+        make_shared<metal>(color{0.8, 0.6, 0.2}, 1.0);
+
+    world.add(make_shared<sphere>(point3{0, -100.5, -1}, 100, material_ground));
+    world.add(make_shared<sphere>(point3{0, 0, -1.2}, 0.5, material_center));
+    world.add(make_shared<sphere>(point3{-1, 0, -1}, 0.5, material_left));
+    world.add(make_shared<sphere>(point3{-1, 0, -1}, 0.4, material_bubble));
+    world.add(make_shared<sphere>(point3{1, 0, -1}, 0.5, material_right));
+
+    // Camera
+    camera cam;
+
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+    cam.vfov = 20;
+    cam.lookfrom = {-2, 2, 1};
+    cam.lookat = {0, 0, -1};
+    cam.vup = {0, 1, 0};
+
+    // Render
+    cam.render(world);
+}
